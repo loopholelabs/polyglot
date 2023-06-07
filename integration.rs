b196c2b67253c591d5c4f1e5da34aae006aa5aa5
Shivansh Vij <shivanshvij@loopholelabs.io>
2023-06-07 13:34:45 -0700

    Fixing rust linting issues, bumping `current_version` file
    Signed-off-by: Shivansh Vij <shivanshvij@loopholelabs.io>

--- integration.rs
+++ integration.rs
@@ -1,3 +1,19 @@
+/*
+ * 	Copyright 2023 Loophole Labs
+ *
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 		   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ */
+
 #[cfg(test)]
 mod tests {
     use crate::decoder::Decoder;
@@ -107,13 +123,17 @@ mod tests {
                 Kind::F32 => {
                     let val = decoder.decode_f32().unwrap();
 
-                    assert!((val as f32 - td.decoded_value.as_f64().unwrap() as f32) < f32::EPSILON);
+                    assert!(
+                        (val as f32 - td.decoded_value.as_f64().unwrap() as f32) < f32::EPSILON
+                    );
                 }
 
                 Kind::F64 => {
                     let val = decoder.decode_f64().unwrap();
 
-                    assert!((val as f64 - td.decoded_value.as_f64().unwrap() as f64) < f64::EPSILON);
+                    assert!(
+                        (val as f64 - td.decoded_value.as_f64().unwrap() as f64) < f64::EPSILON
+                    );
                 }
 
                 Kind::Array => {
